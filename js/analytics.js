b76fcae86e25e70bbee2b9f0c08444eeb788e5ce
ISROisTHEbest <taksheelshah2009@gmail.com>
2026-01-17 16:18:36 +0530

    V3.1.1

---
+++ js/analytics.js
@@ -0,0 +1,11 @@
+
+const script = document.createElement('script');
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-4RKE7J4MR6";
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+
+gtag('js', new Date());
+gtag('config', 'G-4RKE7J4MR6');
